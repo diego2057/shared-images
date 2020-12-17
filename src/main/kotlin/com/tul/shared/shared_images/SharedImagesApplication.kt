a4eeb33aaea270fe0578392f5d0d85f6b62e7224
santiago_rocha <santiago.rocha@tul.com.co>
2020-12-17 15:18:52 -0500

    Added sonarqube and ktlint plugins

--- src/main/kotlin/com/tul/shared/shared_images/SharedImagesApplication.kt
+++ src/main/kotlin/com/tul/shared/shared_images/SharedImagesApplication.kt
@@ -6,6 +6,4 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class SharedImagesApplication
 
-fun main(args: Array<String>) {
-	runApplication<SharedImagesApplication>(*args)
-}
+fun main(args: Array<String>) { runApplication<SharedImagesApplication>(*args) }
