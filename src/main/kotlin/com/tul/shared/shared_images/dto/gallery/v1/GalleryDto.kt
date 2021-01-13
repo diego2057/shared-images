570bf8a4857d56fbb2c5dc92e925bd7bcecf5ea4
santiago_rocha <santiago.rocha@tul.com.co>
2021-01-13 17:33:43 -0500

    Added Swagger

--- src/main/kotlin/com/tul/shared/shared_images/dto/gallery/v1/GalleryDto.kt
+++ src/main/kotlin/com/tul/shared/shared_images/dto/gallery/v1/GalleryDto.kt
@@ -1,8 +1,8 @@
 package com.tul.shared.shared_images.dto.gallery.v1
 
-import com.tul.shared.shared_images.model.Image
+import com.tul.shared.shared_images.dto.image.v1.ImageDto
 
 class GalleryDto {
     var uuid: String? = null
-    var images: List<Image>? = null
+    var images: List<ImageDto>? = null
 }
