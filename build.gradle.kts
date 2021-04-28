8bcd5cefb100ab795320287bee983785a22dd3fc
Jesus Zapata <jzapata@tul.com.co>
2021-04-28 18:53:08 -0500

    Adding com.gorylenko.gradle-git-properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	id("org.sonarqube") version "3.0"
 	id("org.jmailen.kotlinter") version "3.3.0"
+	id("com.gorylenko.gradle-git-properties") version "2.2.4"
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
 	kotlin("kapt") version "1.4.20"
