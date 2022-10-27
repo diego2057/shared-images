619584bf0f0be54831a091faebb46c59d7b2e755
Diego Muñoz Rodríguez <diego.rodriguez@soytul.com>
2022-10-27 13:58:55 +0000

    Merged in feature/OST-436-backend---crear-exponer-endpoint (pull request #91)
    Feature/OST-436 backend   crear exponer endpoint
    
    * se agrega controller para exponer endpoint para obtener los datos de una imagen por id
    
    * se agrega controller para exponer endpoint para obtener los datos de una imagen por id
    
    * se agrega controller para exponer endpoint para obtener los datos de una imagen por id
    
    * se simplifica codigo
    
    
    Approved-by: Cristiam Mercado
    Approved-by: Eduardo Solis
    Approved-by: Diego Iván Valencia Velázquez

---
+++ src/main/kotlin/com/tul/shared/shared_images/controller/global/v1/GlobalController.kt
@@ -0,0 +1,27 @@
+package com.tul.shared.shared_images.controller.global.v1
+
+import com.tul.shared.shared_images.dto.image.v1.ImageDto
+import com.tul.shared.shared_images.dto.image.v1.ImageMapper
+import com.tul.shared.shared_images.service.image.ImageService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import reactor.core.publisher.Mono
+
+@RestController("global.v1")
+@RequestMapping("_global/v1/backoffice")
+class GlobalController(
+    private val imageService: ImageService,
+    private val imageMapper: ImageMapper,
+) {
+
+    @GetMapping("/images/{id}")
+    fun show(@PathVariable id: String): Mono<ResponseEntity<ImageDto>> =
+        imageService.findById(id)
+            .switchIfEmpty(Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND)))
+            .map { ResponseEntity.ok().body(imageMapper.toDto(it)) }
+}
---
+++ src/test/kotlin/com/tul/shared/shared_images/controller/global/v1/GlobalControllerTest.kt
@@ -0,0 +1,77 @@
+package com.tul.shared.shared_images.controller.global.v1
+
+import com.tul.shared.shared_images.configuration.TestConfiguration
+import com.tul.shared.shared_images.configuration.TinifyMock
+import com.tul.shared.shared_images.dto.image.v1.ImageDto
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.MediaType
+import org.springframework.http.client.MultipartBodyBuilder
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
+import java.util.UUID
+
+@SpringBootTest(classes = [TestConfiguration::class])
+@ExtendWith(SpringExtension::class)
+@AutoConfigureWebTestClient
+@DirtiesContext
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GlobalControllerTest {
+
+    @Autowired
+    private lateinit var client: WebTestClient
+
+    private var tinifyMock = TinifyMock(8090)
+
+    @BeforeAll
+    fun loadMock() {
+        tinifyMock.startMockServer()
+    }
+
+    @AfterAll
+    fun shutDownMock() {
+        tinifyMock.stop()
+    }
+
+    @Test
+    fun findImageByIdTest() {
+        val uuid = UUID.randomUUID().toString()
+        val bodyBuilder = MultipartBodyBuilder()
+        bodyBuilder.part("image", ClassPathResource("test.png"), MediaType.MULTIPART_FORM_DATA)
+        bodyBuilder.part("uuid", uuid)
+
+        val imageDto = client.post()
+            .uri("/v1/images")
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(ImageDto::class.java).returnResult().responseBody
+
+        client.get()
+            .uri("/_global/v1/backoffice/images/$uuid")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .jsonPath("uuid").isEqualTo(imageDto!!.uuid!!)
+            .jsonPath("url").isEqualTo(imageDto.url!!)
+    }
+
+    @Test
+    fun notFoundImageByIdTest() {
+        Thread.sleep(100)
+        client.get()
+            .uri("/_global/v1/backoffice/images/${UUID.randomUUID()}")
+            .exchange()
+            .expectStatus().isNotFound
+    }
+}
